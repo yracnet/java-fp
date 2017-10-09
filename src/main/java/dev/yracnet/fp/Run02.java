fc1ec4eb4ab02d788513c02823f65979872c5d29
Willyams Yujra <yracnet@gmail.com>
2017-10-09 14:33:31 -0400

    !Run02+v1

---
+++ src/main/java/dev/yracnet/fp/Run02.java
@@ -0,0 +1,39 @@
+/*
+ * JAVA Functional Programming (JAVA-FP)
+ * htp://www.github.com/yracnet/java-fp
+ */
+package dev.yracnet.fp;
+
+import dev.yracnet.fp.modelo.Cliente;
+import dev.yracnet.fp.modelo.Cliente.Genero;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Willyams Yujra <yracnet@gmail.com>
+ */
+public class Run02 {
+
+	public static void main(String arg[]) {
+		Cliente a = new Cliente("Gonzalo", "gonzo@gmail.com", Genero.MASCULINO);
+		Cliente b = new Cliente("Ruben", "ruben@hotmail.com", Genero.MASCULINO);
+		Cliente c = new Cliente("Rolando", "rolando@gmail.com", Genero.MASCULINO);
+		Cliente d = new Cliente("Ximena", "ximena@hotmail.com", Genero.FEMENINO);
+		List<Cliente> clienteList = Arrays.asList(a, b, c, d);
+		clienteList = filtrarMasculino(clienteList);
+		clienteList.forEach(System.out::println);
+	}
+
+	private static List<Cliente> filtrarMasculino(List<Cliente> clienteList) {
+		List<Cliente> resultList = new ArrayList<>();
+		for(Cliente cliente : clienteList){
+			if(cliente.getGenero() == Genero.MASCULINO){
+				resultList.add(cliente);
+			}
+		}
+		return resultList;
+	}
+
+}
